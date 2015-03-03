cb4cb8e333a177831d4dd503cd83a4b5770dcf76
Billaferd <ritchie.brent@gmail.com>
2015-03-02 20:14:39 -0500

    Added a few more unit tests.

--- tests/src/tests.js
+++ tests/src/tests.js
@@ -31,6 +31,29 @@ pavlov.specify("Mtheory Cover Tests", function(){
                 assert(interval.toDecimal()).equals(numerator / denominator);
             });
 
+            given([1.0136432647705078]).
+            it("Should give a correct ratio from a decimal representation", function(ratio) {
+                var interval = mtheory.interval.fromDecimal(ratio);
+
+                assert(interval.numerator).equals(531441);
+                assert(interval.denominator).equals(524288);
+            });
+
+            given([1]).
+            it("Should give a correct ratio from a decimal representation", function(ratio) {
+                var interval = mtheory.interval.fromDecimal(ratio);
+
+                assert(interval.numerator).equals(1);
+                assert(interval.denominator).equals(1);
+            });
+
+            given([23.46001038464901]).
+            it("Should give a correct ratio from cents", function(cents) {
+                var interval = mtheory.interval.fromCents(cents);
+
+                assert(interval.numerator).equals(531441);
+                assert(interval.denominator).equals(524288);
+            });
         });
     });
 });
